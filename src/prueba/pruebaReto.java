4c756bb043a8a1eef7fb954ca9bf117a1603d3cd
Laura <laurisco023@gmail.com>
2022-11-14 00:55:00 -0500

    suma y resta de dos numeros

--- src/prueba/pruebaReto.java
+++ src/prueba/pruebaReto.java
@@ -1,7 +1,31 @@
 package prueba;
 
+import java.util.Scanner;
+
 public class pruebaReto {
 	public static void main(String[]args){
 		System.out.println("Texto en la clase pruebaReto");
+		int num1, num2, operar, suma, resta; 
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Ingrese el número 1: ");
+		num1= sc.nextInt();
+		
+		System.out.print("Ingrese el número 2: ");
+		num2= sc.nextInt();
+		
+		System.out.print("Seleccione la operación que desea realizar: ");
+		System.out.print("1.Suma ");
+		System.out.print("2.Resta ");
+		operar = sc.nextInt();
+		
+		if(operar == 1) {
+			suma = num1 + num2;
+			System.out.println("El resultado es: " + suma);
+		}
+		else{
+			resta = num1 - num2;
+			System.out.println("El resultado es: " + resta);
+		}
 	}
 }
